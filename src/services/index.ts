ed6bc96db6613cc3a818891566d0d3e55cad7a36
Denis <1911.denis55@gmail.com>
2022-04-11 15:00:40 +0300

    Update index.ts

--- src/services/index.ts
+++ src/services/index.ts
@@ -3,6 +3,6 @@ import axios from 'axios';
 export const api = axios.create({
   baseURL: 'https://api.github.com/',
   headers: {
-    Authorization: `token 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`,
+    Authorization: `Bearer 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`,
   },
 });
