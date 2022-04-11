cce0cb5e3a3ef26c5b7629bf9fb1973e7f4861e6
Denis <1911.denis55@gmail.com>
2022-04-11 15:02:50 +0300

    Update index.ts

--- src/services/index.ts
+++ src/services/index.ts
@@ -3,6 +3,6 @@ import axios from 'axios';
 export const api = axios.create({
   baseURL: 'https://api.github.com/',
   headers: {
-    Authorization: `Bearer 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`,
+    Authorization: `Bearer ${process.env.GH_TOKEN}`,
   },
 });
