4e464814a66d47aa8a01747a3df8d6f44d04a0e5
Denis <1911.denis55@gmail.com>
2022-04-11 17:11:46 +0300

    fix .env

--- src/services/index.ts
+++ src/services/index.ts
@@ -3,6 +3,7 @@ import axios from 'axios';
 export const api = axios.create({
   baseURL: 'https://api.github.com/',
   headers: {
-    Authorization: `Bearer ${process.env.GH_TOKEN}`,
+    Authorization: `Bearer ${process.env.REACT_APP_GH_TOKEN}`,
   },
 });
+console.log(process.env);
